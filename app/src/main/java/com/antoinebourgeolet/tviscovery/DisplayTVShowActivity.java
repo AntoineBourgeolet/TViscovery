9bcbb41ca4e1cf809f74177caafecf6d32b2f26d
Antoine Bourgeolet <a.bourgeolet@gmail.com>
2019-01-27 13:44:05 +0100

    Start adding xml

---
+++ app/src/main/java/com/antoinebourgeolet/tviscovery/DisplayTVShowActivity.java
@@ -0,0 +1,13 @@
+package com.antoinebourgeolet.tviscovery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayTVShowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_tvshow);
+    }
+}
